71d093be37ff5e451c452998a33a738dec33daca
blancopado <pablo.dominguezblanco@gmail.com>
2015-12-06 23:15:39 +0100

    karma tests done, everything working

--- test/toDoChallengeController.spec.js
+++ test/toDoChallengeController.spec.js
@@ -9,25 +9,68 @@ describe('toDoChallengeController', function() {
     ctrl = $controller('ToDo', {
     	$scope: scope
     });
+    scope.clearAgenda();
+    scope.clearListDone();
   }));
-
-  // beforeEach(inject(function($controller) {
-  //   ctrl = $controller('ToDo');
-  // }));
 
   it('initialises with an empty list', function() {
     expect(scope.agenda.length).toBe(0);
 	});  
 
- //  it('initialises with an empty search result and term', function() {
-	// 	//expect(ctrl.addToList).toBeUndefined();
- //    expect(scope.ctrl.agenda.length).toBe(0);
-	// });
+	it('add activity to the list', function() {
+		scope.newActivity = 'Test';
+		scope.addToAgenda();
+		expect(scope.agenda.length).toBe(1);
+    expect(scope.agenda[0]).toBe('Test');
+	});
 
-	// it('add activity to the list', function() {
-	// 	scope.newActivity = 'Test';
-	// 	scope.addToAgenda();
-	// 	expect(scope.ctrl.agenda.length).toBe(1);
-	// });
+  it('deletes all items', function() {
+    scope.newActivity = 'Test';
+    scope.addToAgenda();
+    expect(scope.agenda.length).toBe(1);
+    scope.clearAgenda();
+    expect(scope.agenda.length).toBe(0);
+  });
 
+  it('deletes just one item from the list', function() {
+    scope.newActivity = 'Test';
+    scope.addToAgenda();
+    expect(scope.agenda.length).toBe(1);
+    scope.newActivity = 'Test2';
+    scope.addToAgenda();
+    expect(scope.agenda.length).toBe(2);
+    scope.removeAct(1);
+    expect(scope.agenda.length).toBe(1);
+  });
+
+  it('moves tasks to the list of done activities when deleting from the main list', function() {
+    scope.newActivity = 'Test';
+    scope.addToAgenda();
+    scope.removeAct(0);
+    expect(scope.listDone.length).toBe(1);
+  });
+
+  it('deletes all items from the list of tasks completed', function() {
+    scope.newActivity = 'Test';
+    scope.addToAgenda();
+    scope.removeAct(0);
+    scope.newActivity = 'Test2';
+    scope.addToAgenda();
+    scope.removeAct(0);
+    expect(scope.listDone.length).toBe(2);
+    scope.clearListDone();
+    expect(scope.listDone.length).toBe(0);
+  });
+
+  it('deletes just one item from the list of tasks completed', function() {
+    scope.newActivity = 'Test';
+    scope.addToAgenda();
+    scope.removeAct(0);
+    scope.newActivity = 'Test2';
+    scope.addToAgenda();
+    scope.removeAct(0);
+    expect(scope.listDone.length).toBe(2);
+    scope.removeActDone(1);
+    expect(scope.listDone[0]).toBe('Test');
+  });
 });
